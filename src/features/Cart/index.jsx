9b51b320d0e9acc5c12e88bc607211fd8343e645
GaryHuu <nguyenhuu220800@gmail.com>
2021-10-07 12:04:47 +0700

    feat(source): add modal login register

--- src/features/Cart/index.jsx
+++ src/features/Cart/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch, useRouteMatch } from 'react-router';
+import { Redirect, Route, Switch, useRouteMatch } from 'react-router';
 
 import CartEmpty from './components/CartEmpty';
 import CartHeader from './components/CartHeader';
@@ -10,32 +10,39 @@ import PaymentSuccess from './components/PaymentSuccess';
 
 function Cart() {
   const isCartEmpty = false;
+  const isLoggedIn = true;
   const { url } = useRouteMatch();
   return (
-    <section className='cart'>
-      <div className='container'>
-        <div className='cart__left'>
-          <Switch>
-            <Route path={`${url}`} exact>
-              <CartHeader />
-              {isCartEmpty ? <CartEmpty /> : <CartProductList />}
-            </Route>
-            <Route path={`${url}/confirm`} exact>
-              <CartLocationConfirm />
-            </Route>
-            <Route path={`${url}/success`} exact>
-              <PaymentSuccess />
-            </Route>
-          </Switch>
-        </div>
+    <>
+      {!isLoggedIn ? (
+        <Redirect to='/' />
+      ) : (
+        <section className='cart'>
+          <div className='container'>
+            <div className='cart__left'>
+              <Switch>
+                <Route path={`${url}`} exact>
+                  <CartHeader />
+                  {isCartEmpty ? <CartEmpty /> : <CartProductList />}
+                </Route>
+                <Route path={`${url}/confirm`} exact>
+                  <CartLocationConfirm />
+                </Route>
+                <Route path={`${url}/success`} exact>
+                  <PaymentSuccess />
+                </Route>
+              </Switch>
+            </div>
 
-        {isCartEmpty || (
-          <div className='cart__right'>
-            <CartTotal />
+            {isCartEmpty || (
+              <div className='cart__right'>
+                <CartTotal />
+              </div>
+            )}
           </div>
-        )}
-      </div>
-    </section>
+        </section>
+      )}
+    </>
   );
 }
 
