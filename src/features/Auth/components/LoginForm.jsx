78e1069b1913aa53cccfdd9b0e2d63a3c7b958fa
GaryHuu <nguyenhuu220800@gmail.com>
2021-10-07 18:02:56 +0700

    feat(source): add ui login regiter

--- src/features/Auth/components/LoginForm.jsx
+++ src/features/Auth/components/LoginForm.jsx
@@ -1,7 +1,67 @@
+import { yupResolver } from '@hookform/resolvers/yup';
 import React from 'react';
+import { useForm } from 'react-hook-form';
+import * as yup from 'yup';
+
+import InputField from 'components/form-controls/InputField';
+import PasswordField from 'components/form-controls/PasswordField';
 
 function LoginForm(props) {
-  return <input />;
+  const { onSubmit } = props;
+  const schema = yup.object().shape({
+    identifier: yup
+      .string()
+      .required('Please enter your email')
+      .email('Please enter a valid email'),
+
+    password: yup.string().required('Please enter your password'),
+  });
+
+  const form = useForm({
+    defaultValues: {
+      identifier: '',
+      password: '',
+    },
+    resolver: yupResolver(schema),
+  });
+
+  const handleSubmit = (values) => {
+    if (!onSubmit) return;
+    onSubmit(values);
+  };
+
+  return (
+    <div className='section-login'>
+      <h3 className='section-login__title'>Đăng Nhập</h3>
+      <form onSubmit={form.handleSubmit(handleSubmit)}>
+        <InputField
+          placeholder='Nhập email hoặc số điện thoại'
+          name='identifier'
+          form={form}
+          label='Email / SĐT'
+        />
+        <PasswordField
+          placeholder='Mật khẩu từ 6 đến 32 ký tự'
+          name='password'
+          form={form}
+          label='Mật Khẩu'
+        />
+        <p className='forget-password'>
+          Quên mật khẩu? Nhấn vào
+          <strong> đây</strong>
+        </p>
+        <button className='button-submit' type='submit'>
+          Đăng nhập
+        </button>
+        <button className='button-submit button-submit--fb'>
+          Đăng nhập bằng Facebook
+        </button>
+        <button className='button-submit button-submit--gg'>
+          Đăng nhập bằng Google
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default LoginForm;
