e3cd6c45bf4d8e3a18df241ca5c89d745f30c2b9
HuuNguyen2K <honghuu.nguyen@gumiviet.com>
2021-11-03 22:15:53 +0700

    style order status

--- src/features/Admin/components/AdminOrder/components/EditOrderForm.jsx
+++ src/features/Admin/components/AdminOrder/components/EditOrderForm.jsx
@@ -5,7 +5,7 @@ import moment from 'moment';
 import { DatePicker } from 'antd';
 import { statusOrder } from 'constant';
 import { Select } from 'antd';
-import { useReactToPrint } from "react-to-print";
+import { useReactToPrint } from 'react-to-print';
 const { Option } = Select;
 
 const STATUS_ORDER = [
@@ -36,7 +36,7 @@ function EditOrderForm(props) {
   const componentRef = useRef();
   const handlePrint = useReactToPrint({
     content: () => componentRef.current,
-    pageStyle: `@page { size: 12in ${12 + typeCount*2}in }`,
+    pageStyle: `@page { size: 12in ${12 + typeCount * 2}in }`,
   });
 
   function handleChange(value) {
@@ -85,75 +85,80 @@ function EditOrderForm(props) {
   });
 
   return (
-    <div ref={componentRef} className='edit-modal-content'>
-      <h3>Chi tiết đơn hàng</h3>
-      <div className='code'>
-        <p>
-          Mã đơn hàng:&nbsp;
-          <span>{data.id}</span>
-        </p>
-        <Button onClick={handlePrint}>IN</Button>
+    <div>
+      <div ref={componentRef} className='edit-modal-content'>
+        <h3>Chi tiết đơn hàng</h3>
+        <div className='code'>
+          <p>
+            Mã đơn hàng:&nbsp;
+            <span>{data.id}</span>
+          </p>
+          <Button onClick={handlePrint}>IN</Button>
+        </div>
+        <label>Thời gian đặt hàng: </label>
+        <TimePicker
+          defaultValue={moment(new Date(data.date_order * 1000))}
+          size='large'
+          disabled
+        />
+        <DatePicker
+          disabled
+          size='large'
+          defaultValue={moment(new Date(data.date_order * 1000), 'DD/MM/YYYY')}
+          format={'DD/MM/YYYY'}
+        />
+        <InputField label='Họ tên' name='name' value={data.user.name} />
+        <InputField label='Số điện thoại' name='name' value={data.user.phone} />
+        <InputField label='Email' name='name' value={data.user.email} />
+        <InputField
+          label='Địa chỉ'
+          name='name'
+          value={data.user.address.street_name}
+        />
+        <InputField
+          label='Xã/Phường'
+          name='name'
+          value={data.user.address.ward}
+        />
+        <InputField
+          label='Quận/Huyện'
+          name='name'
+          value={data.user.address.district}
+        />
+        <InputField
+          label='Tỉnh/TP'
+          name='name'
+          value={data.user.address.province}
+        />
+        {productList}
+        <div>
+          <p className='item'>
+            Tổng số: &nbsp;
+            <span>
+              {data.order_details.reduce(
+                (acc, item) => acc + item.product_quantity,
+                0
+              )}
+            </span>
+            &nbsp;sản phẩm
+          </p>
+        </div>
+        <div>
+          <p className='item total'>
+            Tổng số tiền: &nbsp;
+            <span>
+              {data.total.toLocaleString('it-IT', {
+                style: 'currency',
+                currency: 'VND',
+              })}
+            </span>
+          </p>
+        </div>
       </div>
-      <label>Thời gian đặt hàng: </label>
-      <TimePicker
-        defaultValue={moment(new Date(data.date_order * 1000))}
-        size='large'
-        disabled
-      />
-      <DatePicker
-        disabled
-        size='large'
-        defaultValue={moment(new Date(data.date_order * 1000), 'DD/MM/YYYY')}
-        format={'DD/MM/YYYY'}
-      />
-      <InputField label='Họ tên' name='name' value={data.user.name} />
-      <InputField label='Số điện thoại' name='name' value={data.user.phone} />
-      <InputField label='Email' name='name' value={data.user.email} />
-      <InputField
-        label='Địa chỉ'
-        name='name'
-        value={data.user.address.street_name}
-      />
-      <InputField
-        label='Xã/Phường'
-        name='name'
-        value={data.user.address.ward}
-      />
-      <InputField
-        label='Quận/Huyện'
-        name='name'
-        value={data.user.address.district}
-      />
-      <InputField
-        label='Tỉnh/TP'
-        name='name'
-        value={data.user.address.province}
-      />
-      {productList}
-      <div>
-        <p className='item'>
-          Tổng số: &nbsp;
-          <span>
-            {data.order_details.reduce(
-              (acc, item) => acc + item.product_quantity,
-              0
-            )}
-          </span>
-          &nbsp;sản phẩm
-        </p>
-      </div>
-      <div>
-        <p className='item total'>
-          Tổng số tiền: &nbsp;
-          <span>
-            {data.total.toLocaleString('it-IT', {
-              style: 'currency',
-              currency: 'VND',
-            })}
-          </span>
-        </p>
-      </div>
-      <div>
+      <div style={{
+        padding: '10px 30px',
+        borderTop: '1px solid #ccc'
+      }}>
         <label>Trạng thái đơn hàng: </label>
         <Select
           defaultValue={data.status}
