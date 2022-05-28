4553faab31aac649583174ff90552e3fc0963b49
GaryHuu <nguyenhuu220800@gmail.com>
2022-05-28 11:57:12 +0700

    fix: fix bug page

--- src/features/Product/components/ProductLoading.jsx
+++ src/features/Product/components/ProductLoading.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 function ProductLoading(props) {
   return (
     <div className='product-page'>
-      <SkeletonProduct />
+      <SkeletonProduct key={new Date()} />
     </div>
   );
 }
