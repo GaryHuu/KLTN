70e839e6b21aedc78f855ee99c48b10f304ff75e
GaryHuu <nguyenhuu220800@gmail.com>
2022-08-03 23:34:33 +0700

    fix: ui and validate message

--- src/features/Auth/components/Register.jsx
+++ src/features/Auth/components/Register.jsx
@@ -19,7 +19,6 @@ function Register(props) {
       toast.success('Đặng ký thành công!');
       setMode(MODE.LOGIN);
     } catch (error) {
-      // console.log(error);
       toast.error('Đặng ký thất bại!');
     }
     hideLoading();
--- src/features/Auth/components/RegisterForm.jsx
+++ src/features/Auth/components/RegisterForm.jsx
@@ -1,30 +1,33 @@
-import { yupResolver } from '@hookform/resolvers/yup';
-import React from 'react';
-import { useForm } from 'react-hook-form';
-import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup'
+import React from 'react'
+import { useForm } from 'react-hook-form'
+import * as yup from 'yup'
 
-import InputField from 'components/form-controls/InputField';
-import PasswordField from 'components/form-controls/PasswordField';
+import InputField from 'components/form-controls/InputField'
+import PasswordField from 'components/form-controls/PasswordField'
 
 function RegisterForm(props) {
-  const { onSubmit } = props;
+  const { onSubmit } = props
   const schema = yup.object().shape({
     name: yup.string().required('Please enter your full name'),
     email: yup
       .string()
       .required('Please enter your email')
       .email('Please enter a valid email'),
-    password: yup.string().required('Please enter your password').min(6),
+    password: yup
+      .string()
+      .required('Please enter your password')
+      .min(6, 'Password must be at least 6 characters'),
     password_confirmation: yup
       .string()
       .required('Please enter your password')
-      .min(6)
+      .min(6, 'Password must be at least 6 characters')
       .oneOf([yup.ref('password')], 'Password does not match'),
     phone: yup
       .string()
       .required('Please enter your phone number')
       .matches(/^(0[3|5|7|8|9])+([0-9]{8})$/, 'Phone number is not valid'),
-  });
+  })
 
   const form = useForm({
     defaultValues: {
@@ -35,12 +38,12 @@ function RegisterForm(props) {
       phone: undefined,
     },
     resolver: yupResolver(schema),
-  });
+  })
 
   const handleSubmit = (values) => {
-    if (!onSubmit) return;
-    onSubmit(values);
-  };
+    if (!onSubmit) return
+    onSubmit(values)
+  }
 
   return (
     <div className='section-login'>
@@ -76,10 +79,6 @@ function RegisterForm(props) {
           form={form}
           label='Số điện thoại'
         />
-        <p className='forget-password'>
-          <input type='checkbox' />
-          Nhận các thông tin và chương trình khuyến mãi qua email
-        </p>
         <button className='button-submit' type='submit'>
           Đăng ký
         </button>
@@ -89,7 +88,7 @@ function RegisterForm(props) {
         </p>
       </form>
     </div>
-  );
+  )
 }
 
-export default RegisterForm;
+export default RegisterForm
